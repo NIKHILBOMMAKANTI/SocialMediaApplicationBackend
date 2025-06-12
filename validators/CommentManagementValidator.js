44c28423b51d8a9e653083636342479154dd057f
NIKHILBOMMAKANTI <nikhilbommakanti2001@gmail.com>
2025-06-12 11:34:05 +0530

    Updated the Error Messag

--- validators/CommentManagementValidator.js
+++ validators/CommentManagementValidator.js
@@ -4,13 +4,13 @@ const addcommentvalidator = [
       .notEmpty()
       .withMessage("Comment should not be Empty")
       .isLength({ min: 20})
-      .withMessage("Comment should be between 20 to 300 characters Long")
+      .withMessage("Comment must be at least 20 characters long.")
 ];
 const addreplyvalidator = [
     body("reply")
       .notEmpty()
       .withMessage("Comment should not be Empty")
       .isLength({ min: 20})
-      .withMessage("Comment should be between 20 to 300 characters Long")
+      .withMessage("Reply must be at least 20 characters long.")
 ]
 module.exports = {addcommentvalidator,addreplyvalidator}
